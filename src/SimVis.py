96df22676aeb639a3afca6d74d693aa4f958a243
David F Moore <damo@sas.upenn.edu>
2013-06-15 12:53:48 -0400

    Addes src/SimVis.

--- src/SimVis.py
+++ src/SimVis.py
@@ -1,56 +1,17 @@
+import Distributions
 import numpy as np
-from scipy.special import erfc,erfcinv
 
 c = 0.3 # m/ns
+twopi = 2.*np.pi
 
-class Dist:
-    def __init__(self,
-                bm,
-                aL, bL, aH, bH,
-                Smin, So, Smax,
-                PImu, PIsig, PImax)
-        #observation definitions
-        self.bm = bm
+class SimVis:
+    def __init__(self, DistObject, BeamObject, bl, fqs):
+        self.prms = DistObject.SimParams()
+        self.beam = BeamObject
+        self.bl = bl
+        self.fqs = fqs
+        self.l2 = (c/fqs)**2
 
-        #params for flux distriubtions
-        self.aL = aL
-        self.bL = bL
-        self.aH = aH
-
-        #flux limits and turnover
-        self.Smin = Smin
-        self.So = So
-        self.Smax = Smax
-
-        #pol fraction distribution parameters
-        self.PImu = PImu
-        self.PIsig = PIsig
-        self.PImax = PImax
-
-        #useful things to know...
-        self.FindNsrc()
-
-    def FindNsrc(self):
-        dNdS_L = (self.aL/(1-self.bL))*(self.So**(1-self.bL))*(1.-(self.Smin/self.So)**(1-self.bL))
-        dNdS_H = (self.aH/(1-self.bH))*(self.Smax**(1-self.bH))*(1.-(self.So/self.Smax)**(1-self.bH))
-        self.Nsrc = int((dNdS_L+dNdS_H)*self.bm)
-
-    def DrawSrcFlux(self):
-        Xo = (self.bm/self.Nsrc)*(self.aL/(1.-self.bL))*(self.So**(1.-self.bL))*(1.-(self.Smin/self.So)**(1.-self.bL))
-        def lo(x):
-            Si = self.Smin*(1.+(self.Smin**(self.bL-1.))*((1.-self.bL)/self.aL)*(self.Nsrc/self.bm)*x)**(1./(1.-self.bL))
-            return Si
-        def hi(x):
-            Si = self.So*(1.+(self.So**(self.bH-1.))*((1.-self.bH)/self.aH)*(self.Nsrc/self.bm)*(x-Fo))**(1./(1.-self.bH))
-            return Si
-        X = np.random.uniform(0,1,self.Nsrc)
-        S = np.where(FS >= Xo, hi(FS), lo(FS))
-        return S
-
-    def DrawPolFrac(self):
-        Ex = np.log(mu)
-        Sx = np.sqrt(np.log(1.+(self.PIsig/self.PImu)**2))
-        X = np.random.uniform(0,1,self.Nsrc)
-        X *= 0.5*erfc(-1.*(np.log(self.PImax)-Ex)/(Sx*np.sqrt(2.)))
-        X = Ex - np.sqrt(2.)*Sx*erfcinv(2.*X)
-        return np.exp(X)
+    def RMspec(self, flx, nu, rm, pa, s, bl):
+        phs = (rm/np.pi)*self.l2 + (nu/c)*(bl*s[0]) + pa
+        return flx * np.exp(-2.j*np.pi * phs)
