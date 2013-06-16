bd7254e25c26649412a7ed8c6f0c110a6175359b
David F Moore <damo@sas.upenn.edu>
2013-06-16 11:57:54 -0400

    Added the simulaiton to SimVis

--- src/SimVis.py
+++ src/SimVis.py
@@ -5,13 +5,26 @@ c = 0.3 # m/ns
 twopi = 2.*np.pi
 
 class SimVis:
-    def __init__(self, DistObject, BeamObject, bl, fqs):
+    def __init__(self, DistObject, BeamObject, bl, fqs, mfreq=0.15):
         self.prms = DistObject.SimParams()
         self.beam = BeamObject
         self.bl = bl
         self.fqs = fqs
         self.l2 = (c/fqs)**2
 
-    def RMspec(self, flx, nu, rm, pa, s, bl):
-        phs = (rm/np.pi)*self.l2 + (nu/c)*(bl*s[0]) + pa
-        return flx * np.exp(-2.j*np.pi * phs)
+    def spindex(self,G):
+        return (self.mfreq/self.fqs)**G
+
+    def RMspec(self, flx, rm, pa, L, G):
+        phs = (rm/np.pi)*self.l2 + (self.fqs/c)*(self.bl*_L) + pa
+        return flx * self.spindex(G) * np.exp(-2.j*np.pi * phs)
+
+    def SimVis(self):
+        prms = self.prms
+        vis = None
+        for F,L,G,P,RM,X in zip(prms['F'],prms['L'],prms['G'],prms['P'],prms['RM'],prms['X']):
+            if vis is None:
+                vis = P * self.RMspec(F, RM, X, L, G)
+            else:
+                vis += P * self.RMspec(F, RM, X, L, G)
+        return vis
