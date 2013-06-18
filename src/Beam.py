c57e9f987010426f065afabe45f1678c889e92c1
David F Moore <damo@sas.upenn.edu>
2013-06-17 20:30:55 -0400

    Initialized HPXbeam object in src/Beam.py

--- src/Beam.py
+++ src/Beam.py
@@ -1,5 +1,6 @@
 import aipy as a
 import numpy as np
+import healpy as hp
 
 class BeamError(Exception):
     def __init__(self, value):
@@ -11,11 +12,27 @@ class AipyBeam:
     def __init__(self,aa):
         self.aa = aa
     def Response(self, l, m, pol):
-        z = np.sqrt(1.-l**2+m**2)
+        z = np.sqrt(1.-l**2-m**2)
         aa.ants[0].set_active_pol(pol[0])
         BM = aa.ants[0].bm_response((L[i],M[i],z))[:,0]**2
         BM /= aa.ants[0].bm_response((0,0,1))[:,0]**2
         return BM
+
+class HPXBeam:
+    def __init__(self,hpx_alms):
+        self.alms = hpx_alms
+        self.bmx = hp.alm2map(hpx_alms)
+        self.bmy = self.rotate_xy(self.bmx)
+    def Response(self, l, m, pol):
+        z = np.sqrt(1.-l**2-m**2)
+        if pol == 'xx':
+            return self.bmx
+        elif pol == 'yy':
+            return self.bmy
+        px = hp.vec2pix(hp.get_nside(self.map), l, m, z)
+        return self.map[px]
+    def rotate_xy(self):
+        return None
 
 class Beam:
     def __init__(self, fromalm=False, fromaipy=False, hpx_alms=None, aa=None):
@@ -28,6 +45,6 @@ class Beam:
             if hpx_alms is None:
                 raise(BeamError("Pass an array of healpix ALMs!"))
             else:
-                self.beam = HPXBeam(aa)
+                self.beam = HPXBeam(hpx_alms)
         else:
             raise(BeamError("Beam needs to come from aipy or from a healpix alm!"))
